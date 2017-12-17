41b74b79b39d6bf2a524f8c39305c7a56521a903
5-4 <github email address>
2017-12-17 10:19:31 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,4 @@ class Show < ActiveRecord::Base
 	has_many :characters
 	belongs_to :network
 	has_many :actors, through: :characters
-
-	def build_network(params)
-		network = Network.find_or_create_by(params)
-		network.shows << self
-		network
-	end
-
 end
--- db/migrate/008_add_network_id_to_shows.rb
+++
@@ -1,5 +0,0 @@
-class AddNetworkIdToShows < ActiveRecord::Migration
-	def change
-		add_column :shows, :network_id, :string
-	end	
-end
